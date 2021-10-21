06fba35f794a98e0a3ba74a843970da136fff1a7
Tino <tinosirka@gmail.com>
2021-10-21 16:57:44 +0200

    Remove token

--- src/main.js
+++ src/main.js
@@ -6,8 +6,6 @@ import axios from "axios";
 
 axios.defaults.baseURL = "https://api.github.com";
 
-axios.defaults.headers.common["Authorization"] = "token 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"; 
-
 Vue.config.productionTip = false
 
 new Vue({
